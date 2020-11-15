147bdb065f591a6ad2e49b8229fa77850cf36251
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-11-15 12:23:00 +0700

    login -> sign up

--- login/login.js
+++ login/login.js
@@ -2,15 +2,17 @@ document.getElementById("submit-btn").addEventListener("click", function () {
   const username = document.getElementById("user").value;
   const password = document.getElementById("pass").value;
   let checkUser = validateUsername(username);
+  let checkPass = validatePassword(password);
   if (checkUser != "") {
     document.getElementById("error").innerHTML = checkUser;
-  } else {
+  } else if(checkPass != "") {
+    document.getElementById("error").innerHTML = checkPass;
   }
 });
 
 function validateUsername(str) {
-  var error = "";
-  var illegalChars = /\W/; // allow letters, numbers, and underscores
+  let error = "";
+  let illegalChars = /\W/; // allow letters, numbers, and underscores
 
   if (str == "") {
     error = "Please enter Username";
@@ -20,6 +22,14 @@ function validateUsername(str) {
     error = "Use only numbers and alphabets";
   } else {
     error = "";
+  }
+  return error;
+}
+
+function validatePassword(str){
+  let error = "";
+  if(str.length < 8){
+    error = "Password must have more than 8 characters";
   }
   return error;
 }
